895d9cebd8572875bedb21784bd7cfa795f6ea34
Thiranjaya Siriwardhana <kanujathiranjaya@gmail.com>
2026-08-31 11:11:31 +0530

    feat: initialize root layout with metadata and global font configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
             text like <span className="material-symbols-outlined">) */}
         {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&display=optional"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&display=swap"
           rel="stylesheet"
         />
       </head>
